2e7b13b43b54fdb0f05e577dd8af8f31e1c4535e
leobenTaningco <leotaningco@gmail.com>
2025-06-09 23:38:33 +0800

    fix: random borzoi data declaration

--- api/random-borzoi.js
+++ api/random-borzoi.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const response = await fetch("https://dog.ceo/api/breed/borzoi/images/random")
-    data = await response.json()
+    const data = await response.json()
 
     res.writeHead(302, {
         Location: data.message
